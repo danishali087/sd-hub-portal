2213c145475188e8fde24c16fbe07b1b5482f55c
danishali087 <danishclassnotes@gmail.com>
2024-10-09 11:47:55 +0530

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'signin', pathMatch: 'full'},
+  {path: 'signin', component: SigninComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
