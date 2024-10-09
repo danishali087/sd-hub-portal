59ace6e75b3971e4babb461311efbf91353c4aba
danishali087 <danishclassnotes@gmail.com>
2024-10-09 11:58:43 +0530

    Registration component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
