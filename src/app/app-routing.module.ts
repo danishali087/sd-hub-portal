1e00b04317eaabb30d2f030cff0c9086fb7c7ad3
danishali087 <danishclassnotes@gmail.com>
2024-10-09 12:05:29 +0530

    Add import statement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'signin', pathMatch: 'full'},
