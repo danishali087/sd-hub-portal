55ecd44cc30cc95986ebde1654fec41161021046
danishali087 <danishclassnotes@gmail.com>
2024-10-21 12:19:00 +0530

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { RegistrationComponent } from '../registration/registration.component';
 
 @NgModule({
   declarations: [
